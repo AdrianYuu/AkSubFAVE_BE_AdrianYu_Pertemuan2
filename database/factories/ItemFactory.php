35a12dc669e4b3c628db3e036c0ebabeb6c2f3ba
Adrian Yu <adrianyuu01@gmail.com>
2023-06-07 12:10:37 +0700

    [UPDATE]: Update factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,10 +2,10 @@
 
 namespace Database\Factories;
 
-use Carbon\Carbon;
 use App\Models\Item;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
@@ -14,14 +14,15 @@ class ItemFactory extends Factory
 {
     public function definition(): array
     {
+        $faker = faker::create();
         static $idx = 0;
         return [
-            'name' => Str::random(15),
-            'description' => Str::random(30),
-            'price' => rand(10000, 50000),
+            'name' => $faker->words(2, true),
+            'description' => $faker->sentence(),
+            'price' => $faker->numberBetween(10000, 100000),
             'picture' => $idx++ . '.png',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ];
     }
 }
