48de16633386aa236b72bf9b12ffa043335205b6
Adrian Yu <adrianyuu01@gmail.com>
2023-06-07 12:55:55 +0700

    [UPDATE]: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Item;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ItemSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Item::factory(10)->create();
+        $this->call([
+            ItemSeeder::class,
+        ]);
     }
 }
